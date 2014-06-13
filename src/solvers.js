1fcdc8d94298e79273746fa5c1228255551ee983
Hack Reactor Students <johnw424@gmail.com>
2014-06-12 19:33:02 -0700

    Improve find rooks solution

--- src/solvers.js
+++ src/solvers.js
@@ -14,40 +14,23 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(num) {
-
-  var solution = [];
-
-  //write the recursion function addQueen
-  var addQueen = function(board, queenCount, queenPosition){
-    //update board by placing queen at queenPosition
-    var rowPosition = ((queenPosition - (queenPosition % num)) / num);
-    var colPosition = (queenPosition % num);
-    board.attributes[rowPosition][colPosition] = 1;
-    //check if board is still valid
-    if(!(board.hasAnyColConflicts() || board.hasAnyRowConflicts())){
-      //if check num and queenCount equal then push to the solution
-      if(queenCount === num){
-        solution.push(board.rows());
-        board.attributes[rowPosition][colPosition] = 0;
-      //else call for loop and continue recursion;
-      }else{
-        for (var j = (queenPosition + 1); j < (num*num); j++){
-          addQueen(board, (queenCount + 1), j);
+  var columnConflict = {};
+  var board = new Board({n:num});
+  var placeRook = function(num, row, columnConflict, board){
+    for (var i = 0; i < num; i++){
+      if(!columnConflict[i]) {
+        board.togglePiece(row, i);
+        if(row === num - 1) {
+          return board.rows();
         }
+        columnConflict[i] = true;
+        return placeRook(num, row+1, columnConflict, board);
+        // columnConflict[i] = false;
+        // board.togglePiece(row, i);
       }
-    }else{
-      board.attributes[rowPosition][colPosition] = 0;
     }
   };
-
-  //loop through first possible queen move
-  for (var i = 0; i < num * num; i++){
-    //start the recursion
-    var newBoard = new Board({n:num});
-    addQueen(newBoard, 1, i);
-  }
-
-  return solution[0];
+  return placeRook(num, 0, columnConflict, board);
 };
 
 
@@ -108,6 +91,35 @@ window.findNQueensSolution = function(num) {
   }
 
   return solution[0];
+
+
+//   var solutions = [];
+//   var columnConflict = {};
+//   var majorConflict = {};
+//   var minorConflict = {};
+//   var board = new Board({n:num});
+//   var placeQueen = function(num, row, columnConflict, majorConflict, minorConflict, board){
+//     for (var i = 0; i < num; i++){
+//       if(!(columnConflict[i] || majorConflict[(i - row)] || minorConflict[(i + row)])) {
+//         board.togglePiece(row, i);
+//         if (row === num - 1) {
+//           console.log(board);
+//           solutions.push(board.rows());
+//         }
+//         columnConflict[i] = true;
+//         majorConflict[(i - row)] = true;
+//         minorConflict[(i + row)] = true;
+//         placeQueen(num, row+1, columnConflict, majorConflict, minorConflict, board);
+//         board.togglePiece(row, i);
+//         columnConflict[i] = false;
+//         majorConflict[(i - row)] = false;
+//         minorConflict[(i + row)] = false;
+//       }
+//     }
+//   };
+//   placeQueen(num, 0, columnConflict, majorConflict, minorConflict, board);
+//   console.log(solutions[0]);
+//   return solutions[0];
 };
 
 
@@ -120,8 +132,7 @@ window.countNQueensSolutions = function(num) {
   var columnConflict = {};
   var majorConflict = {};
   var minorConflict = {};
-  console.log(0);
-  var placeRook = function(num, row, columnConflict, majorConflict, minorConflict){
+  var placeQueen = function(num, row, columnConflict, majorConflict, minorConflict){
     for (var i = 0; i < num; i++){
       if(!(columnConflict[i] || majorConflict[(i - row)] || minorConflict[(i + row)])) {
         if (row === num - 1) {
@@ -130,13 +141,13 @@ window.countNQueensSolutions = function(num) {
         columnConflict[i] = true;
         majorConflict[(i - row)] = true;
         minorConflict[(i + row)] = true;
-        placeRook(num, row+1, columnConflict, majorConflict, minorConflict);
+        placeQueen(num, row+1, columnConflict, majorConflict, minorConflict);
         columnConflict[i] = false;
         majorConflict[(i - row)] = false;
         minorConflict[(i + row)] = false;
       }
     }
   };
-  placeRook(num, 0, columnConflict, majorConflict, minorConflict);
+  placeQueen(num, 0, columnConflict, majorConflict, minorConflict);
   return solution;
 };
